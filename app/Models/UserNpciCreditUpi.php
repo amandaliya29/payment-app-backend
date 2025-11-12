bbb941149efa3ef5c513903ac459559dfdbc18dc
amandaliya29 <168665452+amandaliya29@users.noreply.github.com>
2025-11-12 23:50:45 +0530

    Add set pin and get details for NPCI API (#62)

--- app/Models/UserNpciCreditUpi.php
+++ app/Models/UserNpciCreditUpi.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserNpciCreditUpi extends Model
 {
-    //
+    /**
+     * The attributes that should be hidden for arrays or JSON serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'pin_code',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'pin_code' => 'hashed',
+    ];
+
+    /**
+     * Get the user that owns this Credit/UPI account.
+     *
+     * @return BelongsTo<\App\Models\User, self>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
